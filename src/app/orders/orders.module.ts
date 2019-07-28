db59f8e1abdc5547ced267c904ccb3b4332c9759
Shi, Shangyilin <michaelins@163.com>
2019-07-28 20:25:01 +0800

    started order

---
+++ src/app/orders/orders.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { OrdersRoutingModule } from './orders-routing.module';
+import { OrdersPage } from './orders.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    OrdersRoutingModule
+  ],
+  declarations: [OrdersPage]
+})
+export class OrdersPageModule { }
