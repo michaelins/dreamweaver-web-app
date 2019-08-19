74b967e262ae62129e6c4cb689910168ce291a36
Shawn Shi <shawn.shi@pb.com>
2019-08-19 16:44:40 +0800

    add photoswipe

--- src/app/clockin/clockin-record/clockin-record.page.ts
+++ src/app/clockin/clockin-record/clockin-record.page.ts
@@ -1,10 +1,13 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ModalController, LoadingController } from '@ionic/angular';
 import { from } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, toArray, mergeMap, map } from 'rxjs/operators';
 import { ClockinRecordNewComponent } from './clockin-record-new/clockin-record-new.component';
 import { ClockinRecordService, CollectionOfRecord, Record } from './clockin-record.service';
+import * as PhotoSwipe from 'PhotoSwipe';
+import * as PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default';
+import { OssService } from '../../shared/oss.service';
 
 @Component({
   selector: 'app-clockin-record',
@@ -13,6 +16,7 @@ import { ClockinRecordService, CollectionOfRecord, Record } from './clockin-reco
 })
 export class ClockinRecordPage implements OnInit {
 
+  @ViewChild('photoSwipe') photoSwipe: ElementRef;
 
   records: Record[];
   collectionOfRecord: CollectionOfRecord = {};
@@ -23,7 +27,9 @@ export class ClockinRecordPage implements OnInit {
   constructor(
     private http: HttpClient,
     private modalCtrl: ModalController,
-    private clockinRecordService: ClockinRecordService
+    private loadingCtrl: LoadingController,
+    private clockinRecordService: ClockinRecordService,
+    private ossService: OssService
   ) { }
 
   ngOnInit() {
@@ -72,6 +78,121 @@ export class ClockinRecordPage implements OnInit {
   }
 
   onAdd() {
+  }
+
+  openGallery(record: Record) {
+
+    const options = {
+      index: 0
+    };
+
+    from(this.loadingCtrl.create({
+      message: '正在加载...',
+      spinner: 'crescent'
+    })).pipe(
+      switchMap(loading => {
+        return loading.present();
+      }),
+      switchMap(() => {
+        return from(record.imgs);
+      }),
+      mergeMap(img => {
+        return this.ossService.getImageInfo(img).pipe(
+          map(info => {
+            let width, height: number;
+            if (info.ImageWidth.value >= info.ImageHeight.value) {
+              if (info.ImageWidth.value <= 800) {
+                width = info.ImageWidth.value;
+                height = info.ImageHeight.value;
+              } else {
+                width = 800;
+                height = info.ImageHeight.value / info.ImageWidth.value * 800;
+              }
+            } else {
+              if (info.ImageHeight.value <= 800) {
+                width = info.ImageWidth.value;
+                height = info.ImageHeight.value;
+              } else {
+                height = 800;
+                width = info.ImageWidth.value / info.ImageHeight.value * 800;
+              }
+            }
+            return {
+              src: `${img}?x-oss-process=image/resize,m_lfit,h_800,w_800`,
+              msrc: `${img}?x-oss-process=image/resize,m_lfit,h_100,w_100`,
+              w: Math.round(width),
+              h: Math.round(height),
+              title: record.intro
+            };
+          })
+        );
+      }),
+      toArray()
+    ).subscribe(resp => {
+      console.log(resp);
+      const gallery = new PhotoSwipe(this.photoSwipe.nativeElement, PhotoSwipeUI_Default, resp, options);
+      gallery.init();
+    }, error => {
+      console.log(error);
+      this.loadingCtrl.dismiss();
+    }, () => {
+      this.loadingCtrl.dismiss();
+    });
+
+    // from(record.imgs).pipe(
+    //   mergeMap(img => {
+    //     return this.ossService.getImageInfo(img).pipe(
+    //       map(info => {
+    //         let width, height: number;
+    //         if (info.ImageWidth.value >= info.ImageHeight.value) {
+    //           if (info.ImageWidth.value <= 800) {
+    //             width = info.ImageWidth.value;
+    //             height = info.ImageHeight.value;
+    //           } else {
+    //             width = 800;
+    //             height = info.ImageHeight.value / info.ImageWidth.value * 800;
+    //           }
+    //         } else {
+    //           if (info.ImageHeight.value <= 800) {
+    //             width = info.ImageWidth.value;
+    //             height = info.ImageHeight.value;
+    //           } else {
+    //             height = 800;
+    //             width = info.ImageWidth.value / info.ImageHeight.value * 800;
+    //           }
+    //         }
+    //         return {
+    //           src: `${img}?x-oss-process=image/resize,m_lfit,h_800,w_800`,
+    //           w: Math.round(width),
+    //           h: Math.round(height)
+    //         };
+    //       })
+    //     );
+    //   }),
+    //   toArray()
+    // ).subscribe(resp => {
+    //   console.log(resp);
+    // });
+    // Build gallery images array
+    // const images = [
+    //   {
+    //     src: 'https://r08872-dw-dev.oss-cn-zhangjiakou.aliyuncs.com/75a46def995c437288982d1c74f7ca37.jpg?x-oss-process=image/resize,m_fill,h_480,w_480',
+    //     w: 480,
+    //     h: 480
+    //   },
+    //   {
+    //     src: 'https://r08872-dw-dev.oss-cn-zhangjiakou.aliyuncs.com/694427f0de8c4a36a7fe50451961625f.jpg?x-oss-process=image/resize,m_fill,h_480,w_480',
+    //     w: 480,
+    //     h: 480
+    //   },
+    //   {
+    //     src: 'https://r08872-dw-dev.oss-cn-zhangjiakou.aliyuncs.com/9001928efff442e8844486ea12674ab6.jpg?x-oss-process=image/resize,m_fill,h_480,w_480',
+    //     w: 480,
+    //     h: 480
+    //   }
+    // ];
+
+
   }
 
   preview(event) {
--- src/app/shared/oss.service.ts
+++ src/app/shared/oss.service.ts
@@ -1,9 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.prod';
+import { map } from 'rxjs/operators';
 
 export interface UploadImageResponse {
   imgUrl: string;
+}
+
+export interface ImageInfo {
+  FileSize: { value: string };
+  Format: { value: string };
+  ImageHeight: { value: number };
+  ImageWidth: { value: number };
+  Compression?: { value: string };
+  DateTime?: { value: string };
+  ExifTag?: { value: string };
+  GPSLatitude?: { value: string };
+  GPSLatitudeRef?: { value: string };
+  GPSLongitude?: { value: string };
+  GPSLongitudeRef?: { value: string };
+  GPSMapDatum?: { value: string };
+  GPSTag?: { value: string };
+  GPSVersionID?: { value: string };
+  JPEGInterchangeFormat?: { value: string };
+  JPEGInterchangeFormatLength?: { value: string };
+  Orientation?: { value: string };
+  ResolutionUnit?: { value: string };
+  Software?: { value: string };
+  XResolution?: { value: string };
+  YResolution?: { value: string };
 }
 
 @Injectable({
@@ -17,5 +42,15 @@ export class OssService {
 
   uploadImage(formData: FormData) {
     return this.http.post<UploadImageResponse>(`${environment.apiServer}/oss/upload`, formData);
+  }
+
+  getImageInfo(imageUrl: string) {
+    return this.http.get<ImageInfo>(`${imageUrl}?x-oss-process=image/info`).pipe(
+      map(info => {
+        info.ImageWidth.value = +info.ImageWidth.value;
+        info.ImageHeight.value = +info.ImageHeight.value;
+        return info;
+      })
+    );
   }
 }
