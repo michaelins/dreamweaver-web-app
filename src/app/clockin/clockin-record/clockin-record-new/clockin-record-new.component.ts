2e7a3a45fb2b360ded7b00783d72993c26ac5946
Shawn Shi <shawn.shi@pb.com>
2019-08-23 18:16:04 +0800

    Fixed 80

--- src/app/clockin/clockin-record/clockin-record-new/clockin-record-new.component.ts
+++ src/app/clockin/clockin-record/clockin-record-new/clockin-record-new.component.ts
@@ -4,6 +4,7 @@ import { from } from 'rxjs';
 import { concatMap, map, switchMap, toArray } from 'rxjs/operators';
 import { OssService } from '../../../shared/oss.service';
 import { ClockinRecordService } from '../clockin-record.service';
+import Compressor from 'compressorjs';
 
 @Component({
   selector: 'app-clockin-record-new',
@@ -43,9 +44,46 @@ export class ClockinRecordNewComponent implements OnInit {
     });
 
     from(this.files).pipe(
-      concatMap(file => {
+      switchMap(file => {
+        return from(new Promise<Blob>((resolve, reject) => {
+          const compresser = new Compressor(file, {
+            success: resolve,
+            error: reject,
+            maxHeight: 800,
+            maxWidth: 800,
+            quality: 0.8,
+            convertSize: 2000000
+          });
+        })).pipe(
+          map(result => {
+            console.log(result);
+            return { result, file };
+          })
+        );
+      }),
+      concatMap(result => {
+        console.log(result);
+
+        // const compresser = new Compressor(this.files[0], {
+        //   quality: 0.6,
+        //   success(result) {
+        //     const formData1 = new FormData();
+
+        //     // The third parameter is required for server
+        //     formData1.append('file', result, result.name);
+
+        //     // Send the compressed image file to server with XMLHttpRequest.
+        //     axios.post('/path/to/upload', formData).then(() => {
+        //       console.log('Upload success');
+        //     });
+        //   },
+        //   error(err) {
+        //     console.log(err.message);
+        //   },
+        // });
+
         const formData = new FormData();
-        formData.append('file', file, file.name);
+        formData.append('file', result.result, result.file.name);
         return this.ossService.uploadImage(formData);
       }),
       map(resp => {
